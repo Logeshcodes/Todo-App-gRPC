4ad949f6b02cb787dffd4242d56479d7dc668c63
Logesh <logeshc18022002@gmail.com>
2024-12-21 10:59:19 +0530

    start

---
+++ cilent.js
@@ -0,0 +1,46 @@
+const grpc = require("@grpc/grpc-js");
+const protoLoader = require("@grpc/proto-loader");
+
+// Load the .proto file
+const PROTO_PATH = "./todo.proto";
+const packageDefinition = protoLoader.loadSync(PROTO_PATH, {
+  keepCase: true,
+  longs: String,
+  enums: String,
+  defaults: true,
+  oneofs: true,
+});
+const todoProto = grpc.loadPackageDefinition(packageDefinition).todo;
+
+// Create a client instance
+const client = new todoProto.TodoService(
+  "localhost:5000",
+  grpc.credentials.createInsecure()
+);
+
+// Create a new To-Do
+client.CreateTodo({ title: "Learn gRPC", description: "Build a To-Do app with gRPC" }, (err, response) => {
+  if (err) {
+    console.error("Error creating To-Do:", err);
+    return;
+  }
+  console.log("Created To-Do:", response);
+});
+// client.CreateTodo({ title: "Learn Node", description: "Build a big project" }, (err, response) => {
+//   if (err) {
+//     console.error("Error creating To-Do:", err);
+//     return;
+//   }
+//   console.log("Created To-Do:", response);
+// });
+
+
+
+// Fetch all To-Dos
+client.GetTodos({}, (err, response) => {
+  if (err) {
+    console.error("Error fetching To-Dos:", err);
+    return;
+  }
+  console.log("To-Do List:", response.todos);
+});
---
+++ server.js
@@ -0,0 +1,48 @@
+const grpc = require("@grpc/grpc-js");
+const protoLoader = require("@grpc/proto-loader");
+
+// Load the .proto file
+const PROTO_PATH = "./todo.proto";
+const packageDefinition = protoLoader.loadSync(PROTO_PATH, {
+  keepCase: true,
+  longs: String,
+  enums: String,
+  defaults: true,
+  oneofs: true,
+});
+const todoProto = grpc.loadPackageDefinition(packageDefinition).todo;
+
+// In-memory database for todos
+const todos = [];
+let idCounter = 1;
+
+// Implement the service methods
+const todoService = {
+  CreateTodo: (call, callback) => {
+    const todo = {
+      id: String(idCounter++),
+      title: call.request.title,
+      description: call.request.description,
+    };
+    todos.push(todo);
+    callback(null, todo);
+  },
+
+  GetTodos: (_, callback) => {
+    callback(null, { todos });
+  },
+};
+
+// Start the gRPC server
+const server = new grpc.Server();
+server.addService(todoProto.TodoService.service, todoService);
+
+const PORT = "5000";
+server.bindAsync(`0.0.0.0:${PORT}`, grpc.ServerCredentials.createInsecure(), (err) => {
+  if (err) {
+    console.error("Server error:", err);
+    return;
+  }
+  console.log(`Server running at http://0.0.0.0:${PORT}`);
+  server.start();
+});
